df02d71f70777f09818f5e8e8a399391b5e88558
Daria Kovtun <dashakovtun27@gmail.com>
2018-09-30 22:02:39 +0300

    Resolve conflicts

--- Doodler.py
+++ Doodler.py
@@ -2,24 +2,24 @@ import pygame
 
 
 class Doodler(pygame.sprite.Sprite):
-	jumpCount = 10
-	x = 600
-	y = 600
-	isJump = False
-	speed = 5
-	image = 0
+    jumpCount = 10
+    x = 600
+    y = 600
+    isJump = False
+    speed = 25 #скорость дудлера по горизонтали
+    image = 0
 
-	def __init__(self):
-		super(Doodler, self).__init__()
-		self.image = pygame.image.load('static/dog_right.png')
-		image = self.image
-		self.rect = self.image.get_rect()
+    def __init__(self):
+        super(Doodler, self).__init__()
+        self.image = pygame.image.load('static/dog_right.png')
+        image = self.image
+        self.rect = self.image.get_rect()
 
-	def update(self,win):
-		keys = pygame.key.get_pressed()
-		if keys[pygame.K_LEFT]:
-			self.x -= self.speed
-			self.image = pygame.image.load('static/dog_left.png')
-		if keys[pygame.K_RIGHT]:
-			self.x += self.speed
-			self.image = pygame.image.load('static/dog_right.png')
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x -= self.speed
+            self.image = pygame.image.load('static/dog_left.png')
+        if keys[pygame.K_RIGHT]:
+            self.x += self.speed
+            self.image = pygame.image.load('static/dog_right.png')
