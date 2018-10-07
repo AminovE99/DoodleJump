4a9ee49a6b528292d3773ff07a5420dd6cd3cb32
Daria Kovtun <dashakovtun27@gmail.com>
2018-10-07 20:22:45 +0300

    Add cropped sprites

--- doodler.py
+++ doodler.py
@@ -10,7 +10,7 @@ class Doodler(pygame.sprite.Sprite):
 
     def __init__(self):
         super(Doodler, self).__init__()
-        self.image = pygame.image.load('static/dog_right.png')
+        self.image = pygame.image.load('static/dog2smallright.png')
         self.rect = self.image.get_rect()
         self.jump = 10
         self.gravity = 0
@@ -20,10 +20,10 @@ class Doodler(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and self.x > 0:
             self.x -= self.speed
-            self.image = pygame.image.load('static/dog_left.png')
+            self.image = pygame.image.load('static/dog2smallleft.png')
         if keys[pygame.K_RIGHT] and self.x < 600:
             self.x += self.speed
-            self.image = pygame.image.load('static/dog_right.png')
+            self.image = pygame.image.load('static/dog2smallright.png')
 
         if not self.jump:
             self.y += self.gravity
